489219815f4b3b6e00640eaeb3c352a30b31b624
hussain-manalodi <hussainmanalodi@gmail.com>
2020-06-22 10:31:22 +0530

    First commit

--- lib/main.dart
+++ lib/main.dart
@@ -11,9 +11,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-
         primarySwatch: Colors.blue,
-
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home: MyHomePage(title: 'Flutter Demo Home Page'),
